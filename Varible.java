14d2d022767f315615a7edb697ff2cf46a2d2143
SoumyaranjanRout7 <routsoumyaranjan2000@gmail.com>
2021-05-14 09:42:10 +0530

    First Commit

---
+++ Varible.java
@@ -0,0 +1,16 @@
+/**
+ * Varibales are a way to store information in our computer.
+ * Variables that we define in a program can be accessed by a name we give them
+ * 
+ * To define a variable we need to specify the daya type,then give our variable a name,
+ * and optionally add an expression to intitialize the variable with a value
+ */
+
+public class Varible {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        // ! myFirstNumber is a variable of type int(Integer)
+        System.out.println(myFirstNumber);
+    }
+}
